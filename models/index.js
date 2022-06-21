f55cb3df5bd0dd586bd4f28b04da77c31c3530f8
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-06-21 10:06:36 -0700

    added assosiations

--- models/index.js
+++ models/index.js
@@ -5,13 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_ids'
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tagg'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.hasMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tagging'
+})
 module.exports = {
   Product,
   Category,
